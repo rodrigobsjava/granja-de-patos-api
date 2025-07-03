8277461e681cb46e483696bba200f1b1b696828f
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2025-07-03 18:59:32 -0400

    chore: estrutura inicial do projeto criada

---
+++ src/main/java/com/rodrigobs/granja_de_patos_api/GranjaDePatosApiApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigobs.granja_de_patos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GranjaDePatosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GranjaDePatosApiApplication.class, args);
+	}
+
+}
