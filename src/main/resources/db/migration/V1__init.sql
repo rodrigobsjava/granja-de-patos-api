8f47b89a1ea57bf2e0d0ee19b9dc813cfd75adce
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2025-07-03 22:29:53 -0400

    feat(setup): configuração inicial com Spring Boot, Docker, Flyway e PostgreSQL

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,52 @@
+-- ===============================================
+-- 	Flyway - Script Inicial do Banco - PostgreSQL
+-- ===============================================
+
+-- CLIENTE
+CREATE TABLE cliente (
+    id UUID PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    tem_desconto BOOLEAN NOT NULL
+);
+
+-- VENDEDOR
+CREATE TABLE vendedor (
+    id UUID PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    cpf VARCHAR(14) NOT NULL UNIQUE,
+    matricula VARCHAR(20) NOT NULL UNIQUE
+);
+
+-- PATO
+CREATE TABLE pato (
+    id UUID PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    nome_mae VARCHAR(100),
+    pato_mae_id UUID,
+    vendido BOOLEAN NOT NULL DEFAULT FALSE,
+    CONSTRAINT fk_pato_mae FOREIGN KEY (pato_mae_id) REFERENCES pato(id)
+);
+
+
+-- VENDA
+CREATE TABLE venda (
+    id UUID PRIMARY KEY,
+    cliente_id UUID NOT NULL,
+    vendedor_id UUID NOT NULL,
+    data_venda TIMESTAMP NOT NULL,
+    valor_total NUMERIC(10, 2) NOT NULL,
+
+    CONSTRAINT fk_venda_cliente FOREIGN KEY (cliente_id) REFERENCES cliente(id),
+    CONSTRAINT fk_venda_vendedor FOREIGN KEY (vendedor_id) REFERENCES vendedor(id)
+);
+
+-- VENDA_PATOS (tabela associativa)
+CREATE TABLE venda_patos (
+    venda_id UUID NOT NULL,
+    pato_id UUID NOT NULL,
+
+    PRIMARY KEY (venda_id, pato_id),
+
+    CONSTRAINT fk_venda_pato_venda FOREIGN KEY (venda_id) REFERENCES venda(id),
+    CONSTRAINT fk_venda_pato_pato FOREIGN KEY (pato_id) REFERENCES pato(id)
+);
